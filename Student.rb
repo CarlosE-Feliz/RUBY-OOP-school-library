2b7024eeb0b80951cc424eab13ba7db6b216b298
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-01-24 15:11:05 -0400

    create student class

---
+++ Student.rb
@@ -0,0 +1,9 @@
+class Student>Person
+def initialize(classroom, age, name = 'Unknown', parent_permision: true)
+    @classroom = classroom
+    super(age, name, parent_permision)
+end
+    def play_hooky 
+        "¯\(ツ)/¯"
+    end
+end
